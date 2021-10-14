6ed0c3572908261b7c2bdb24e634a7eaee9d20b9
Erick Martinez <erickmartinezhuaylla123@gmail.com>
2021-10-14 00:41:02 -0400

    Error Dialog

--- app/src/main/java/edu/upb/cocinaya/ui/login/fragments/loginErrror/LoginErrorFragment.kt
+++ app/src/main/java/edu/upb/cocinaya/ui/login/fragments/loginErrror/LoginErrorFragment.kt
@@ -1,13 +1,14 @@
-package edu.upb.cocinaya.ui.fragments
+package edu.upb.cocinaya.ui.login.fragments.loginErrror
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import edu.upb.cocinaya.R
 
-class LoginErrorFragment : Fragment() {
+class LoginErrorFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
